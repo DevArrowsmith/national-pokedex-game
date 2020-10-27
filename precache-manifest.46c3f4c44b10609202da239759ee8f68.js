d9e69863d3309260afeb0b34e16b5528edb57d69
GummiCode <gummicodeblog@gmail.com>
2020-10-27 14:27:06 +0000

    Updates

---
+++ precache-manifest.46c3f4c44b10609202da239759ee8f68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b5224cf7323af11445039ada152406a",
+    "url": "/national-pokedex-game/index.html"
+  },
+  {
+    "revision": "ca53a5b2d46b9cfb4ff5",
+    "url": "/national-pokedex-game/static/js/2.0050bb11.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/national-pokedex-game/static/js/2.0050bb11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2e5a12eea621bb39aa6",
+    "url": "/national-pokedex-game/static/js/main.3aaa46ed.chunk.js"
+  },
+  {
+    "revision": "c8de8d498d292ea992a0",
+    "url": "/national-pokedex-game/static/js/runtime-main.7b5d86c5.js"
+  }
+]);
